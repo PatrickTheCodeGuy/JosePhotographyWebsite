56a361a9ba021a1f87cabccc8668a919dd5726cf
Patrick Thompson <patrick.jthompson93@gmail.com>
2018-09-29 19:01:55 -0700

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<div className="footer-container">
+			<p className="footer-p">&copy; 2018 JOSE PAQUIAN PHOTOGRAPHY</p>
+		</div>
+	);
+}
+
+export default Footer;
---
+++ src/components/Page Header/PageHeaderContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function PageHeader() {
+	return (
+		<div className="headerContainer">
+			<h1 className="header-h1">Jose Paquian Photography</h1>
+		</div>
+	);
+}
+
+export default PageHeader;
