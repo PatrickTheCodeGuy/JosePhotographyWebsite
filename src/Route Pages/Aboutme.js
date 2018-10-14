a8e950a3984957bed54a0e161fbdf245f2fd5af2
PatrickTheCodeGuy <patrick.jthompson93@gmail.com>
2018-10-13 21:48:46 -0700

    added basic about me page

--- src/Route Pages/Aboutme.js
+++ src/Route Pages/Aboutme.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
 import NavBar from "../components/NavBar/NavBarContainer";
 import PageHeader from "../components/Page Header/PageHeaderContainer";
+import profilePhoto from "../media/Jose Photo.jpg";
+import Footer from "../components/Footer/Footer";
 
 function AboutMe() {
 	return (
 		<div className="App">
 			<PageHeader />
 			<NavBar />
-			<h1>Hi there!</h1>
+			<div className="AboutmeContainer">
+				<div className="image-container">
+					<img className="img" src={profilePhoto} />
+				</div>
+				<div className="aboutme-description">
+					<p className="paragraph">
+						Hello there! My name is Jose Paquian, and welcome to my page! I have
+						been taking photos for over 10+ years. The type of photos I have
+						experience in taking are:{" "}
+					</p>
+					<ul className="list">
+						<li>Cars</li>
+						<li>Nature</li>
+						<li>Band Events</li>
+					</ul>
+					<p className="paragraph">
+						If you are interested in contacting me for work, head on over to my
+						contact page.
+					</p>
+				</div>
+			</div>
+			<Footer />
 		</div>
 	);
 }
